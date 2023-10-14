94b31f9a18afa3549a80b750e581e9f367294b40
Corey Alix <coreyalix@gmail.com>
2023-10-14 01:31:52 +0000

    Building tokenPrinter

--- matrix/data/tokens/index.ts
+++ matrix/data/tokens/index.ts
@@ -1,43 +1,61 @@
 const NONE = "none";
 
 import { farmPack } from "../cards/farmpack.js";
-import { DamageBonus, Health } from "../cards/pack_base.js";
+import { DamageBonus, Health, Movement, RangeDirections } from "../cards/pack_base.js";
 
-const farmerCard = farmPack.find(c => c.name === "Farmer")!;
+const farmTokens = farmPack.map(c => ({
+    name: c.name,
+    health: c.health,
+    movement: c.movement,
+}));
 
-const farmerToken = {
-    name: farmerCard.name,
-    health: farmerCard.health,
-    movement: farmerCard.movement,
+type Token =  {
+    name: string | undefined;
+    health: Health | undefined;
+    movement: Movement | null | undefined;
 }
-
-type Token = typeof farmerToken;
 
 
 export function run() {
     const printer = new TokenPrinter(document.querySelector("body")!);
-    printer.print(farmerToken);
+    printer.print(farmTokens);
 }
 
 class TokenPrinter {
     constructor(private target: HTMLElement) { }
 
-    print(token: Token) {
-        const name = token.name;
-        const health = this.printHealth(token.health);
-        const movement = token.movement;
-
-        const template = `
-        <div class="token">
-        <span>Token:</span><span>${name}<span>
-        <span>Health:</span><span>${health}<span>
-        <span>Movement:</span><span>${movement}<span>
-        </div>
-        `
-
-        this.target.innerHTML = template;
+    print(tokens: Token[]) {
+        const template = tokens.map(t => this.printToken(t)).join("");
+        this.target.innerHTML = `<div class="tokens">${template}</div>`;
     }
 
+    printToken(token: Token) {
+        const name = token.name;
+        const health = this.printHealth(token.health);
+        const movement = this.printMovement(token.movement!);
+
+        return `
+        <div class="token">
+        <span class="label value center larger">${name}</span>
+        <span class="label">Health:</span><span class="value">${health}</span>
+        <span class="label">Movement:</span><span class="value">${movement}</span>
+        </div>
+        `
+    }
+
+    printMovement(movement?: Movement) {
+        if (!movement) return NONE;
+        const direction = this.printDirection(movement.direction);
+        return `<div class="directions">${direction}</div>:${movement.distance}`
+      }
+
+      printDirection(direction: string | RangeDirections[]): string {
+        if (!direction) return NONE;
+        if (typeof direction === "string") return `<div class="direction ${direction}">${direction}</div>`;
+        return direction.map(d => this.printDirection(d)).join("");
+      }
+        
+    
     private printHealth(health?: Health) {
         if (!health) return NONE;
         if (typeof health == "string") return health;
